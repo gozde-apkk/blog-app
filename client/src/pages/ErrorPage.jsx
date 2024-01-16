d2d4aacfd02d500198b03773c4239dd74b90371b
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-16 20:56:42 +0300

    errorPage.jsx

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const ErrorPage = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='error-page'>
+      <div className='center'>
+        <Link to='/' className='btn primary'>
+        <h2>Page Not Found</h2>P
+        </Link>
+      </div>
+    </section>
   )
 }
 
