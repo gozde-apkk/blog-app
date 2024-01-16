42be85a79ae0ac39c02fe45dbd2b3c6e8b1a68a6
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-16 20:56:22 +0300

    postItem.jsx

---
+++ client/src/components/PostItem.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PostAuthor from './PostAuthor'
+const PostItem = ({title, thumbnail, description, postId, category, authorID}) => {
+  const shortDescription = description .lenght > 145 ? description.substr(0, 145)+ '...' : description;
+  const postTitle = title      .lenght > 145 ? title    .substr(0, 145)+ '...' : title    ;
+
+  return (
+    <article style={{width : "19rem", height:"26rem"}}>
+      <div className="post_thumbnail">
+        <img src={thumbnail} alt={title}/>
+      </div>
+      <div className="post_contnt">
+       <Link to={`/posts/${postId}`}> <h3>{postTitle}</h3></Link>
+        <p>{shortDescription}</p>
+        <div className="post_footer">
+          <PostAuthor/>
+          <Link className='btn category' to={`/posts/categories/${category}`}>{category} </Link>
+        </div>
+      </div>
+    </article>
+  )
+}     
+
+export default PostItem
