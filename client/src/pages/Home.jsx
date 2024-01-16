8174e14af27441efeee5dea181a6c20ac723e98b
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-16 20:56:57 +0300

    home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Posts from '../components/Posts'
+
+
+
 
 const Home = () => {
+  
+
   return (
     <div>
-      
+      <Posts/>  
     </div>
   )
 }
