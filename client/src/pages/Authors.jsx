ba65a2cea1e519f231605315d6037590e0803e30
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-17 12:25:59 +0300

    authors.jsx created

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -6,7 +6,7 @@ import Avatar3 from "../images/avatar3.jpg";
 import Avatar5 from "../images/avatar5.jpg";
 import Avatar4 from "../images/avatar4.jpg";
 import { Link } from "react-router-dom";
-
+import '../index.css'
 const authorData = [
   {
     id: 1,
@@ -41,23 +41,25 @@ const authorData = [
 ];
 const Authors = () => {
   const [authors, setAuthors] = useState(authorData);
-  return 
-    <section className="authors">
-      {authors.length > 0 ? 
+  return (
+    <section style={{paddingLeft: '9rem', paddingBlockStart:'2rem'}} className="authors">
+      {authors.length > 0 ? (
         <div className="container authors_container">
-          {authors.map(({ id, name, avatar, posts }) => {
-            return
+          {authors.map(({ id, name, avatar, posts }) => (
             <Link key={id} to={`/posts/users/${id}`}>
-              <div className="aut   hor_avator">
+              <div className="author_avatar">
                 <img src={avatar} alt={`Image of ${name}`} />
               </div>
               <div className="author_info">
                 <h5>{name}</h5>
                 <p>{posts} posts</p>
               </div>
-            </Link>;
-              })}
-            </div> :         <h2>No users/authors found </h2>}
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <h2>No users authors found </h2>
+      )}
     </section>
   );
 };
