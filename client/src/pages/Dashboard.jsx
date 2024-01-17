49632557f71fc997915e45ea7a17e470a9f73a0c
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-17 13:34:39 +0300

    dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Link} from 'react-router-dom'; 
+import {DUMMY_POSTS} from '../data'
+import '../index.css'
 const Dashboard = () => {
+
+  const [posts , setPosts] = useState(DUMMY_POSTS);
   return (
-    <div>
-      
-    </div>
+    <section className='dashboard'>
+      {
+        posts.length ? <div className="container dashboard_container">
+          {
+            posts.map((post) => {
+              return <article key={post.id} className='dashboard_post'>
+                <div className="dashboard_post-info">
+                  <div className="dashboard_post-thumbnail">
+                    <img src={post.thumbnail}  />
+                  </div>
+                  <p>{post.title} </p>
+                </div>
+                <div className="dashboard_post-actions">
+                  <Link to={`/posts/${post.id}`} className='btn sm'>View Post</Link>
+                  <Link to={`/posts/${post.id}/edit`} className='btn sm primary'>Edit Post</Link>
+                  <Link to={`/posts/${post.id}/delete`} className='btn sm danger'>Delete Post</Link>
+                </div>
+              </article>
+            })
+          }
+        </div> : <h2 className='center'>You hav no posts yet.</h2>
+      }
+    </section>
   )
 }
 
