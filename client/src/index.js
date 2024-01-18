e9c5306cd687170616a8f27fbb812e04367440d4
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-18 20:35:51 +0300

    blog-app

--- client/src/index.js
+++ client/src/index.js
@@ -1,51 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import ErrorPage from './pages/ErrorPage'
-import PostDetails from './pages/PostDetails';
-import Login from './pages/Login';
-import UserProfile from './pages/UserProfile';
-import Authors from './pages/Authors';
-import CreatePosts from './pages/CreatePosts';
-import CategoryPost from './pages/CategoryPost';
-import AuthorPosts from './pages/AuthorPosts';
-import Dashboard from './pages/Dashboard';
-import EditPost from './pages/EditPost';
-import Register from './pages/Register';
-import DeletePost from './pages/DeletePost';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
+import PostDetails from "./pages/PostDetails";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import Authors from "./pages/Authors";
+import CreatePosts from "./pages/CreatePosts";
+import CategoryPost from "./pages/CategoryPost";
+import AuthorPosts from "./pages/AuthorPosts";
+import Dashboard from "./pages/Dashboard";
+import EditPost from "./pages/EditPost";
+import Register from "./pages/Register";
+import DeletePost from "./pages/DeletePost";
+import UserProvider from './context/userContext'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
-    errorElement: <ErrorPage/>,
-    children : [
-      {index : true, element : <Home/>},
-      {path : "posts/:id", element :<PostDetails/>},
-      {path : "authors", element :<Authors/>},
-      {path : "/login", element : <Login/>},
-      {path : "/register", element : <Register/>},
-      {path : "/profile/:id", element : <UserProfile/>},
-      {path : "/authors", element : <Authors/>},
-      {path : "/create", element : <CreatePosts/>},
-      {path : "/posts/categories/:categories", element : <CategoryPost/>},
-      {path : "/posts/users/:id", element : <AuthorPosts/>},
-      {path : "/myposts/:id", element : <Dashboard/>},
-      {path : "/posts/:id/edit", element : <EditPost/>},
-      {path : "/posts/:id/delete", element : <DeletePost  />},
+    element: (
+      <UserProvider>
+        <Layout />
+      </UserProvider>
+    ),
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "posts/:id", element: <PostDetails /> },
+      { path: "authors", element: <Authors /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "/profile/:id", element: <UserProfile /> },
+      { path: "/authors", element: <Authors /> },
+      { path: "/create", element: <CreatePosts /> },
+      { path: "/posts/categories/:categories", element: <CategoryPost /> },
+      { path: "/posts/users/:id", element: <AuthorPosts /> },
+      { path: "/myposts/:id", element: <Dashboard /> },
+      { path: "/posts/:id/edit", element: <EditPost /> },
+      { path: "/posts/:id/delete", element: <DeletePost /> },
+    ],
+  },
+]);
 
-      
-    ]
-    
-  }
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-        <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
