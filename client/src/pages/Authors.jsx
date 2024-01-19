4bf5f66c2f7c52a8c2b27a44ec037444bf546307
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-20 01:12:46 +0300

    authors page completed

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,54 +1,30 @@
-import React, { useState } from "react";
-
-import Avatar1 from "../images/avatar1.jpg";
-import Avatar2 from "../images/avatar2.jpg";
-import Avatar3 from "../images/avatar3.jpg";
-import Avatar5 from "../images/avatar5.jpg";
-import Avatar4 from "../images/avatar4.jpg";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import '../index.css'
-const authorData = [
-  {
-    id: 1,
-    name: "Ernest Achiever",
-    avatar: Avatar1,
-    posts: 3,
-  },
-  {
-    id: 2,
-    name: "Dramani Mahama",
-    avatar: Avatar2,
-    posts: 0,
-  },
-  {
-    id: 3,
-    name: "Jane Doe",
-    avatar: Avatar5,
-    posts: 4,
-  },
-  {
-    id: 4,
-    name: "Nana Addo",
-    avatar: Avatar3,
-    posts: 4,
-  },
-  {
-    id: 5,
-    name: "Hajia Bintu",
-    avatar: Avatar4,
-    posts: 1,
-  },
-];
+import axios from "axios";
+
 const Authors = () => {
-  const [authors, setAuthors] = useState(authorData);
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+   const getAuthors = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/users/authors");
+      setAuthors(response?.data);
+    } catch (error) {
+      console.log(error);
+    }
+   }
+   getAuthors();
+  },[])
   return (
     <section style={{paddingLeft: '9rem', paddingBlockStart:'2rem'}} className="authors">
       {authors.length > 0 ? (
         <div className="container authors_container">
-          {authors.map(({ id, name, avatar, posts }) => (
+          {authors.map(({ _id : id, name, avatar, posts }) => (
             <Link key={id} to={`/posts/users/${id}`}>
               <div className="author_avatar">
-                <img src={avatar} alt={`Image of ${name}`} />
+                <img src={`http://localhost:5000/uploads/${avatar}`} alt={`Image of ${name}`} />
               </div>
               <div className="author_info">
                 <h5>{name}</h5>
